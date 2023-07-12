169681ffd7c61f2ba2b2c67a9d4dfe4cc7bb058c
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-12 20:11:22 +0400

    make admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Task, Tag
 
-admin.site.register(User, UserAdmin)
+
+class TaskManagerAdminSite(admin.AdminSite):
+    pass
+
+
+task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
+
+
+@admin.register(Tag, site=task_manager_admin_site)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task, site=task_manager_admin_site)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'first_name', 'last_name', 'email', 'role']
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ["role"]}),)
+
+
+task_manager_admin_site.register(User, CustomUserAdmin)
