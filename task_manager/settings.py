e9e7c6a81f1f3a9e4b3bb15e635b5baba38aa675
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-14 14:02:03 +0400

    add django filters

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'rest_framework',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
