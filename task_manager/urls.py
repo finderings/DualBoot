6b6d02b8a3661c1abb6c619f9d3b68569568c4aa
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-14 15:55:04 +0400

    add routing

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from main.admin import task_manager_admin_site
+from main.views import UserViewSet, TaskViewSet, TagViewSet
+
+from rest_framework import routers
+
+
+router = routers.SimpleRouter()
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'tasks', TaskViewSet, basename='tasks')
+router.register(r'tags', TagViewSet, basename='tags')
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
+    path('api/', include(router.urls)),
 ]
