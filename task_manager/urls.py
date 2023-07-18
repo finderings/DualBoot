acac5e88dd4b5b70822878cae2929230b4cdd243
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-18 15:10:45 +0400

    add swagger

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,16 +1,46 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from main.admin import task_manager_admin_site
 from main.views import UserViewSet, TaskViewSet, TagViewSet
 
-from rest_framework import routers
+from rest_framework import permissions, routers
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 
 router = routers.SimpleRouter()
-router.register(r'users', UserViewSet, basename='users')
-router.register(r'tasks', TaskViewSet, basename='tasks')
-router.register(r'tags', TagViewSet, basename='tags')
+router.register(r"users", UserViewSet, basename="users")
+router.register(r"tasks", TaskViewSet, basename="tasks")
+router.register(r"tags", TagViewSet, basename="tags")
 
 urlpatterns = [
     path("admin/", task_manager_admin_site.urls),
-    path('api/', include(router.urls)),
+    path("api/", include(router.urls)),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
