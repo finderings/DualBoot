77a89b7838799cc0cf1bf318ab14a45d50b6a682
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-20 11:41:22 +0400

    edit permissions

--- main/permissions.py
+++ main/permissions.py
@@ -3,8 +3,6 @@ from rest_framework import permissions
 
 class IsStaffDelete(permissions.BasePermission):
     def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        if request.method == "DELETE" and request.user.is_staff:
-            return True
-        return False
+        if request.method == "DELETE":
+            return request.user.is_staff
+        return True
