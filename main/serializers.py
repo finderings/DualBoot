515e5ebf0c6160af7fd46a4e87eefefacdd0a8c1
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-22 12:23:37 +0400

    update user model

--- main/serializers.py
+++ main/serializers.py
@@ -7,7 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'id', 'username', 'first_name', 'last_name', 'email', 
+            'id', 'username', 'first_name', 'last_name',
+            'email', 'date_of_birth', 'phone'
         )
             
 
