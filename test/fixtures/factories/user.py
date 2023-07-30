52e089e70750c190754335b9d576989b3e98fd2d
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-07-30 17:24:38 +0400

    add factories

---
+++ test/fixtures/factories/user.py
@@ -0,0 +1,23 @@
+from factory import Factory, Faker
+
+from main.models import User
+from .base import ImageFileProvider
+
+
+Faker.add_provider(ImageFileProvider)
+
+
+class UserFactory(Factory):
+    username = Faker("user_name")
+    role = Faker("random_element", elements=User.Roles.values)
+    email = Faker("email")
+    first_name = Faker("first_name")
+    last_name = Faker("last_name")
+    date_of_birth = Faker("date")
+
+    class Meta:
+        model = User
+
+
+class SuperUserFactory(UserFactory):
+    is_staff = True
