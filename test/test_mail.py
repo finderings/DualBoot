57f82f9328d1effabaffd08ca5727603f5fe3c81
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-08-01 14:04:48 +0400

    add mail test

---
+++ test/test_mail.py
@@ -0,0 +1,30 @@
+from unittest.mock import patch, MagicMock
+
+from django.core import mail
+from django.template.loader import render_to_string
+
+from main.models import Task
+from main.services.mail import send_assign_notification
+from test.base import TestViewSetBase
+from test.fixtures.factories.user import UserFactory
+from test.fixtures.factories.task import TaskFactory
+
+
+class TestSendEmail(TestViewSetBase):
+    @patch.object(mail, "send_mail")
+    def test_send_assign_notification(self, fake_sender: MagicMock) -> None:
+        performer = UserFactory.create()
+        task = TaskFactory.create(performer=performer)
+
+        send_assign_notification(task.id)
+
+        fake_sender.assert_called_once_with(
+            subject="You've assigned a task.",
+            message="",
+            from_email=None,
+            recipient_list=[performer.email],
+            html_message=render_to_string(
+                "emails/notification.html",
+                context={"task": Task.objects.get(pk=task.id)},
+            ),
+        )
