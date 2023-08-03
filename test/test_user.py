24ae541074856d6ab2abc46665120e3d2af485af
Valeriy Korotovskiy <skeletpro67@gmail.com>
2023-08-03 16:03:58 +0400

    add avatar test

--- test/test_user.py
+++ test/test_user.py
@@ -1,68 +1,131 @@
 from test.base import TestViewSetBase
-import factory
 from test.fixtures.factories.user import UserFactory
+
+from http import HTTPStatus
+
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+import factory
 
 
 class TestUserViewSet(TestViewSetBase):
     basename = "users"
-    user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
-    another_user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
-    user_attributes["first_name"] = "Johnny"
-    another_user_attributes["first_name"] = "Jimmy"
-    del user_attributes["role"]
-    
-    def setUp(self) -> None:
-        super().setUp()
-        self.user = self.create(self.user_attributes)
-        
+     
     @staticmethod
     def expected_details(entity: dict, attributes: dict):
         return {**attributes,
                 "id": entity["id"],
                 "phone": entity["phone"],
+                "avatar_picture": entity["avatar_picture"],
                 }
 
     def test_create(self):
-        expected_response = self.expected_details(
-            self.user,
-            self.user_attributes)
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        user = self.create(user_attributes)
+        del user_attributes["role"]
 
-        assert self.user == expected_response
+        expected_response = self.expected_details(
+            user, user_attributes
+            )
+
+        assert user == expected_response
 
     def test_delete(self):
-        deleted_user = self.delete(self.user["id"])
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        user = self.create(user_attributes)
+
+        deleted_user = self.delete(user["id"])
 
         assert deleted_user == None
 
     def test_retrieve(self):
-        retrieved_data = self.retrieve(self.user["id"])
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        user = self.create(user_attributes)
 
-        assert self.user == retrieved_data
+        retrieved_data = self.retrieve(user["id"])
+
+        assert user == retrieved_data
 
     def test_update(self):
-        updated_data = self.update(
-            self.another_user_attributes, key=self.user["id"]
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        another_user_attributes = factory.build(
+            dict, FACTORY_CLASS=UserFactory
             )
+        user = self.create(user_attributes)
+        user_attributes["first_name"] = "Johnny"
+        another_user_attributes["first_name"] = "Jimmy"
 
-        assert updated_data["first_name"] == "Jimmy"
+        updated_user = self.update(
+            another_user_attributes, key=user["id"]
+            )
+        expected_response = self.expected_details(
+            user, user_attributes
+            )
+        
+        expected_response["avatar_picture"] = updated_user["avatar_picture"]
+
+        assert updated_user["first_name"] == "Jimmy"
+        assert expected_response["avatar_picture"] == updated_user[
+            "avatar_picture"
+            ]
 
     def test_list(self):
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        self.create(user_attributes)
         data = self.list()
 
         assert len(data) == 1
 
     def test_filter(self):
+        user_attributes = factory.build(
+            dict, FACTORY_CLASS=UserFactory, first_name="Johnny"
+            )
+        another_user_attributes = factory.build(
+            dict, FACTORY_CLASS=UserFactory, first_name="Jimmy"
+            )
+        user = self.create(user_attributes)
+        
         data = self.filter({"first_name": "John"})
 
         expected_response_match = self.expected_details(
-            self.user, self.user_attributes
+            user, user_attributes
             )
         expected_response_no_match = self.expected_details(
-            self.user, self.another_user_attributes
+            user, another_user_attributes
             )
+        expected_response_match.pop("role", None)
+        expected_response_no_match.pop("role", None)
 
         assert expected_response_match in data
         assert expected_response_no_match not in data
 
     def test_unauthorized(self):
-        self.check_authorization(self.user["id"])
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        user = self.create(user_attributes)
+        self.check_authorization(user["id"])
+
+    def test_large_avatar(self) -> None:
+        avatar_file = SimpleUploadedFile("large.jpg", b"x" * 2 * 1024 * 1024)
+        user_attributes = factory.build(
+            dict, FACTORY_CLASS=UserFactory, avatar_picture=avatar_file
+            )
+
+        response = self.request_create(user_attributes)
+
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json() == {
+            "avatar_picture": ["Maximum size 1048576 exceeded."]
+            }
+
+    def test_avatar_bad_extension(self) -> None:
+        user_attributes = factory.build(dict, FACTORY_CLASS=UserFactory)
+        user_attributes["avatar_picture"].name = "bad_extension.pdf"
+
+        response = self.request_create(user_attributes)
+        
+        assert response.status_code == HTTPStatus.BAD_REQUEST
+        assert response.json() == {
+            "avatar_picture": [
+                "File extension “pdf” is not allowed. Allowed extensions are: jpeg, jpg, png."
+            ]
+        }
